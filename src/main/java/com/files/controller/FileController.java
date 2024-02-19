38197ca8eace17153ae0705dbe75b19a16c3263a
Pradeep <pk3483899@gmail.com>
2024-02-19 10:57:05 +0530

    Delete and get

--- src/main/java/com/files/controller/FileController.java
+++ src/main/java/com/files/controller/FileController.java
@@ -8,16 +8,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 @RestController
-    @RequestMapping("/file")
+@RequestMapping("/file")
 public class FileController {
 
     @Autowired
@@ -39,5 +38,31 @@ public class FileController {
             throw new RuntimeException(e);
         }
         return new ResponseEntity<>(new FileResponse(fileName,"File  is successfully uploaded"), HttpStatus.OK);
+    }
+
+
+
+    @DeleteMapping("/delete")
+    public ResponseEntity<FileResponse > fileDelete(
+            @RequestParam("image") MultipartFile image
+    ) {
+        String fileName = null;
+        try {
+            fileName = fileService.deleteFile(path, image);
+        } catch (
+                IOException e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(new FileResponse(null, "Image  is not deleted !!"), HttpStatus.INTERNAL_SERVER_ERROR);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return new ResponseEntity<>(new FileResponse(fileName, "File  is Deleted"), HttpStatus.OK);
+    }
+    @GetMapping("/find")
+    public ResponseEntity<String[]> getFiles(){
+//        File f=new File("C:\\Users\\pk348\\OneDrive\\OneDrive\\Desktop\\Numetry\\crudwithfiles\\crudwithfiles\\images\\");
+//        String[] list = f.list();
+        String[] file = fileService.getFile();
+        return new ResponseEntity<>(file, HttpStatus.OK);
     }
 }
--- src/main/java/com/files/service/serviceImpl/FileServiceImpl.java
+++ src/main/java/com/files/service/serviceImpl/FileServiceImpl.java
@@ -1,6 +1,8 @@
 package com.files.service.serviceImpl;
 
 import com.files.service.FileService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -24,9 +26,38 @@ public class FileServiceImpl implements FileService {
         if (!f.exists()){
           f.mkdir();
         }
-        //file copy
 
+        //file copy
         Files.copy(file.getInputStream(), Paths.get(filePath));
         return name;
     }
+
+    @Override
+    public String deleteFile(String path, MultipartFile file) throws Exception {
+        String name = file.getOriginalFilename();
+
+        //full path
+        String filePath = path + File.separator + name;
+
+        //create folder if not created
+        File f = new File(filePath);
+        if (f.exists()){
+            f.delete();
+        }
+        return name;
+    }
+
+    @Override
+    public String[] getFile() {
+        File f=new File("C:\\Users\\pk348\\OneDrive\\OneDrive\\Desktop\\Numetry\\crudwithfiles\\crudwithfiles\\images\\");
+        String[] list = f.list();
+        return list;
+    }
+
+//    public static ResponseEntity<String[]> getFiles(){
+//        File f=new File("C:\\Users\\pk348\\OneDrive\\OneDrive\\Desktop\\Numetry\\crudwithfiles\\crudwithfiles\\images\\");
+//        String[] list = f.list();
+//        return new ResponseEntity<>(list, HttpStatus.OK);
+//    }
+
 }
